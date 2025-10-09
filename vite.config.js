37e274e49a1a1a38bd37227cc1d11220270c1fb7
Ashish <ashishfebtech@gmail.com>
2025-10-09 15:12:22 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/adplus-aqua-premium/',  // ✅ Your exact repo name
+  base: '/adplus-aqua-premium/',
 })
